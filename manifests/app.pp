f7067a0d0e9b1582cf8c7638b2188f96381a1b5c
Andrew Rowson <github@growse.com>
2013-07-20 21:33:03 +0000

    Added final bits to uwsgi

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,56 +1,65 @@
 define uwsgi::app (
-  $ensure         = 'present',
-  $enabled        = true,
-  $vhost          = true,
-  $plugins       = 'python',
-  $http_socket    = undef,
-  $socket         = undef,
-  $master         = true,
-  $enable_threads = true,
-  $processes      = 2,
-  $env            = undef,
-  $pythonpath     = undef,
-  $wsgi_file      = undef,
-  $touch_reload   = undef,
-  $virtualenv     = undef,
-  $chdir          = undef,
-  $uid            = undef,
-  $gid            = undef,
-  $home 	  = undef,
-  $modulename     = undef,
-  $buffersize     = 0
+    $ensure         = 'present',
+    $enabled        = true,
+    $vhost          = true,
+    $plugins       = ['python'],
+    $http_socket    = undef,
+    $socket         = undef,
+    $master         = true,
+    $enable_threads = true,
+    $processes      = 2,
+    $env            = undef,
+    $pythonpath     = undef,
+    $wsgi_file      = undef,
+    $touch_reload   = undef,
+    $virtualenv     = undef,
+    $chdir          = undef,
+    $uid            = undef,
+    $gid            = undef,
+    $home           = undef,
+    $modulename     = undef,
+    $buffersize     = 4096
 ) {
 
-  validate_re($ensure, '^(present|absent)$',
+    validate_re($ensure, '^(present|absent)$',
     'ensure must be "present" or "absent".')
 
-  validate_bool($enabled)
+    validate_bool($enabled)
 
 
-  $available_path = "${uwsgi::app_dir}/apps-available/${name}.ini"
-  $enabled_path = "${uwsgi::app_dir}/apps-enabled/${name}.ini"
+    $available_path = "${uwsgi::app_dir}/apps-available/${name}.ini"
+    $enabled_path = "${uwsgi::app_dir}/apps-enabled/${name}.ini"
 
-  file { $available_path:
-    ensure  => $ensure,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => template('uwsgi/app.erb'),
-  }
+    install_plugins{$plugins:;}
 
-  $enable_ensure = $ensure ? {
-    'present' => $enabled ? {
-        true => 'link',
-        false => 'absent'
-    },
-    default => 'absent'
-  }
+    file { $available_path:
+        ensure  => $ensure,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template('uwsgi/app.erb'),
+        notify  => Service['uwsgi']
+    }
 
-  file { $enabled_path:
-    ensure => $enable_ensure,
-    target => $available_path,
-    require => File[$available_path],
-    notify => Service['uwsgi'],
-  }
+    $enable_ensure = $ensure ? {
+        'present' => $enabled ? {
+            true => 'link',
+            false => 'absent'
+        },
+        default => 'absent'
+    }
 
+    file { $enabled_path:
+        ensure => $enable_ensure,
+        target => $available_path,
+        require => File[$available_path],
+        notify => Service['uwsgi'],
+    }
+
+}
+
+define install_plugins {
+    package{"uwsgi-plugin-$name":
+        ensure => installed
+    }
 }
